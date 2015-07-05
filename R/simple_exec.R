375b03201d1c5879bdc755fd5aeee4feb6939cef
Hiroaki Yutani <yutani.ini@gmail.com>
2015-07-05 08:16:36 -0400

    separate wrap function

--- R/simple_exec.R
+++ R/simple_exec.R
@@ -1,23 +1,35 @@
 # simply run arbitrary script and get result
 library(jsonlite)
 
-simple_exec <- function(params) {
-  warn <- err <- NULL
-  script <- fromJSON(params)$script
 
-  res <- withCallingHandlers(
-           tryCatch(eval(parse(text = script)),
-             error = function(e) {
-               err <<- conditionMessage(e)
-               NULL
-             }),
-             warning = function(w) {
-               warn <<- append(warn, conditionMessage(w))
-               invokeRestart("muffleWarning")
-             })
+wrap_func <- function(fun) {
+  function(params) {
+    warn <- err <- NULL
+    params <- fromJSON(params)
+
+    res <- withCallingHandlers(
+             tryCatch(fun(params),
+               error = function(e) {
+                 err <<- conditionMessage(e)
+                 NULL
+               }),
+               warning = function(w) {
+                 warn <<- append(warn, conditionMessage(w))
+                 invokeRestart("muffleWarning")
+               })
+
+    result_json <- toJSON(list(result = res, warning = warn, error = err),
+                          auto_unbox = TRUE, null = "null")
   
-  result_json <- toJSON(list(result = res, warning = warn, error = err), 
-                        auto_unbox = TRUE, null = "null")
-
-  as.character(result_json)
+    as.character(result_json)
+  }
 }
+
+
+simple_exec_ <- function(params) {
+  paste0(capture.output({
+    eval(parse(text = params$script))
+  }), collapse = "\n")
+}
+
+simple_exec <- wrap_func(simple_exec_)
